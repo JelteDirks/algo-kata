e0167c7c220f4e00a0d5520540f477b2d153fb51
Jelte Dirks <j.dirks@outlook.com>
2023-10-30 22:57:10 +0100

    print args in loop
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  char *buf = malloc(128 * sizeof(char));
+  for (int i = 0; i < argc; i++) {
+    sprintf(buf, "%s\n", argv[i]);
+    fprintf(stdout, "%s", buf);
+  }
+}
