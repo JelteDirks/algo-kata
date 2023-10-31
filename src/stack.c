c8949bb75d4bc2a7382e56086c06023895131a3a
Jelte Dirks <j.dirks@outlook.com>
2023-10-31 22:00:15 +0100

    test main in test file
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/stack.c
+++ src/stack.c
@@ -1,21 +1,7 @@
 #include "stack.h"
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 void push(struct Stack *stack, int data) {}
 
 int pop(struct Stack *stack) { return 0; }
 
 int peek(struct Stack *stack) { return 0; }
-
-int main(int argc, char *argv[]) {
-  char *buf = malloc(128 * sizeof(char));
-
-  for (int i = 0; i < argc; i++) {
-    sprintf(buf, "%s\n", argv[i]);
-    fprintf(stdout, "%s", buf);
-  }
-
-  free(buf);
-}
