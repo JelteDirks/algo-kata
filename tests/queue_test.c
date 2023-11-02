3933aea7c3d257fbad4c1c164bf6768a53d80109
Jelte Dirks <j.dirks@outlook.com>
2023-11-02 21:13:23 +0100

    create tests for queue
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- tests/queue_test.c
+++ tests/queue_test.c
@@ -4,6 +4,70 @@
 // Test case
 START_TEST(test_queue) {
   struct Queue *q = make_queue();
+  int data = -1;
+  int result = -1;
+
+  enqueue(q, 2);
+  enqueue(q, 3);
+  enqueue(q, 4);
+
+  result = dequeue(q, &data);
+  ck_assert_int_eq(data, 2);
+  ck_assert_int_eq(result, 0);
+
+  result = dequeue(q, &data);
+  ck_assert_int_eq(data, 3);
+  ck_assert_int_eq(result, 0);
+
+  result = dequeue(q, &data);
+  ck_assert_int_eq(data, 2);
+  ck_assert_int_eq(result, 0);
+
+  result = dequeue(q, &data);
+  ck_assert_int_eq(result, 1);
+
+  enqueue(q, 10);
+  enqueue(q, 11);
+  enqueue(q, 12);
+  enqueue(q, 13);
+  enqueue(q, 14);
+
+  result = dequeue(q, &data);
+  ck_assert_int_eq(data, 10);
+  ck_assert_int_eq(result, 0);
+
+  result = dequeue(q, &data);
+  ck_assert_int_eq(data, 11);
+  ck_assert_int_eq(result, 0);
+
+  result = dequeue(q, &data);
+  ck_assert_int_eq(data, 12);
+  ck_assert_int_eq(result, 0);
+
+  result = dequeue(q, &data);
+  ck_assert_int_eq(data, 13);
+  ck_assert_int_eq(result, 0);
+
+  result = dequeue(q, &data);
+  ck_assert_int_eq(data, 14);
+  ck_assert_int_eq(result, 0);
+
+  ck_assert_int_eq(q->size, 0);
+
+  for (int i = 0; i < 1000; i++) {
+    enqueue(q, i);
+  }
+
+  ck_assert_int_eq(q->size, 1000);
+
+
+  for (int j = 0; j < 1000; j++) {
+    result = dequeue(q, &data);
+    ck_assert_int_eq(data, j);
+    ck_assert_int_eq(result, 0);
+  }
+
+  ck_assert_int_eq(q->size, 0);
 
   free_queue(q);
 }
