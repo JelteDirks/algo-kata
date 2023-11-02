2acc64132d241e062d2db7e1e66911ffd8104a97
Jelte Dirks <j.dirks@outlook.com>
2023-11-02 21:13:51 +0100

    stub implementation for queue
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/queue.c
+++ src/queue.c
@@ -2,8 +2,20 @@
 #include <stdlib.h>
 
 struct Queue *make_queue() {
-  struct Queue *q = {0};
+  struct Queue *q = malloc(sizeof(struct Queue));
+  q->size = 0;
+  q->capacity = 0;
+  q->internal = malloc(q->capacity * sizeof(int));
   return q;
 }
 
-void free_queue(struct Queue *q) { free(q); }
+void free_queue(struct Queue *q) {
+  free(q->internal);
+  free(q);
+}
+
+void enqueue(struct Queue *q, int data) {}
+
+int dequeue(struct Queue *q, int *data) { return 1; }
+
+int peek(struct Queue *q, int *data) { return 1; }
--- src/queue.h
+++ src/queue.h
@@ -1,9 +1,14 @@
 struct Queue {
   int size;
+
+  // -->
+  int *internal;
+  int capacity;
+  // <--
 };
 
 struct Queue *make_queue();
 void free_queue(struct Queue *q);
 void enqueue(struct Queue *q, int data);
-int *dequeue(struct Queue *q);
-int *peek(struct Queue *q);
+int dequeue(struct Queue *q, int *data);
+int peek(struct Queue *q, int *data);
