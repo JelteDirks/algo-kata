b0b4beb3f88d2f8151dd291218d566798b161983
Jelte Dirks <j.dirks@outlook.com>
2023-12-21 22:27:53 +0100

    add more tests for the heap
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- tests/minheap_test.c
+++ tests/minheap_test.c
@@ -1,20 +1,72 @@
 #include "../src/minheap.h"
 #include <check.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 // Test case
 START_TEST(test_minheap) {
   struct MinHeap *h = make_minheap(10);
 
   insert(h, 2);
-  insert(h, 3);
   insert(h, 4);
-  insert(h, 5);
   insert(h, 6);
+  insert(h, 3);
+  insert(h, 5);
+
+  ck_assert_int_eq(h->size, 5);
 
   ck_assert_int_eq(extract_min(h), 2);
   ck_assert_int_eq(extract_min(h), 3);
   ck_assert_int_eq(extract_min(h), 4);
+  ck_assert_int_eq(extract_min(h), 5);
+
+  ck_assert_int_eq(h->size, 1);
+
+  ck_assert_int_eq(extract_min(h), 6);
+
+  ck_assert_int_eq(h->size, 0);
+
+  insert(h, 100);
+  insert(h, 20);
+  extract_min(h);
+  insert(h, 40);
+  insert(h, 80);
+  insert(h, 80);
+  insert(h, 80);
+  extract_min(h);
+
+  ck_assert_int_eq(extract_min(h), 80);
+  ck_assert_int_eq(extract_min(h), 80);
+  ck_assert_int_eq(extract_min(h), 80);
+  ck_assert_int_eq(extract_min(h), 100);
+
+  ck_assert_int_eq(h->size, 0);
+
+  int n = pow(2, 20);
+
+  for (int i = 0; i < n; i++) {
+    int x_rand = random();
+    insert(h, x_rand);
+  }
+
+  ck_assert_int_eq(h->size, n);
+
+  int comp_arr[n];
+
+  for (int i = 0; i < n; i++) {
+    comp_arr[i] = extract_min(h);
+  }
+
+  for (int i = 1; i < n; i++) {
+    int a = comp_arr[i - 1];
+    int b = comp_arr[i];
+
+    if (a != b) {
+      ck_assert_int_lt(a, b);
+    }
+  }
+
+  ck_assert_int_eq(h->size, 0);
 
   free_minheap(h);
 }
